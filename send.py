9830547b80ae0c826f74b787424bc6fcd1844e5a
Jack Cook <hello@jackcook.nyc>
2017-09-03 20:53:53 -0400

    Load member names and emails from spreadsheet

--- send.py
+++ send.py
@@ -1,6 +1,7 @@
 import boto3
 import json
 from pynliner import Pynliner
+import requests
 
 def get_keys():
     f = open("keys.json", "r")
@@ -20,6 +21,38 @@ def render_template():
 
     p = Pynliner().from_string(html).with_cssString(css)
     return p.run()
+
+class Member:
+    first = ""
+    last = ""
+    email = ""
+
+    def __init__(self, row):
+        self.first = row[1]
+        self.last = row[2]
+        self.email = row[0]
+
+    def __repr__(self):
+        return "%s %s <%s>" % (self.first, self.last, self.email)
+
+def request_members():
+    keys = get_keys()
+    api_key = keys["GOOGLE_API_KEY"]
+    range_ = "A1:C1000"
+    spreadsheet_id = keys["SPREADSHEET_ID"]
+    values = (spreadsheet_id, range_, api_key)
+
+    r = requests.get("https://sheets.googleapis.com/v4/spreadsheets/%s/values/%s?key=%s" % values)
+    members_json = r.json()["values"]
+    members_json.pop(0)
+
+    members = []
+
+    for member_data in members_json:
+        member = Member(member_data)
+        members.append(member)
+
+    return members
 
 client = boto3.client(service_name = "ses", region_name = "us-east-1")
 
